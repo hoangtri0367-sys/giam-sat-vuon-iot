ef1b883ba7300a8e5d2cdd72b9a7e77141740427
hoangtri0367-sys <hoangtri0367@gmail.com>
2026-06-28 07:01:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ app = Flask(__name__)
 DB_PATH         = os.environ.get("DB_PATH", "cai_ngot.db")
 
 # Telegram Bot
-TELEGRAM_TOKEN   = os.environ.get("TELEGRAM_TOKEN",8813861052:AAFAjPXv-4RR66dQQkZ_dMyqybq88YtACTI "")
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID",8916859053 "")
+TELEGRAM_TOKEN   = os.environ.get("TELEGRAM_TOKEN", "")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "")
 
 # Email báo cáo tuần
 EMAIL_SENDER    = os.environ.get("EMAIL_SENDER",    "")
